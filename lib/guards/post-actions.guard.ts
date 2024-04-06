16c94a503e7efb9e87d01ecd899c93715d34163a
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-06 14:17:24 +0300

    Created post guard

---
+++ lib/guards/post-actions.guard.ts
@@ -0,0 +1,29 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+
+@Injectable()
+export class PostActionsGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    try {
+      const request: Request = context.switchToHttp().getRequest();
+      const token = request.headers.authorization.split(' ')[1];
+      if (!token) throw new UnauthorizedException();
+      const user: User = this.jwtService.verify(token);
+      if (user.id !== request.params.id) throw new UnauthorizedException();
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+}
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { UserId } from 'lib/decorators/user-id.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('posts')
 export class PostsController {
   constructor(private postService: PostsService) {}
+  @UseGuards(JwtAuthGuard)
   @Get()
   async test(@UserId() userId: string) {
     return userId;
