8e3863d2d906bbf04da3c7600acb297d3defb4da
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-06 14:02:37 +0300

    created post service CRUD

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [PostsController],
-  providers: [PostsService]
+  providers: [PostsService],
 })
 export class PostsModule {}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsersService } from 'src/users/users.service';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(
+    private userService: UsersService,
+    private prisma: PrismaService,
+  ) {}
+  async getMany(limit: number, page: number) {
+    return this.prisma.post.findMany({
+      include: { author: true },
+      skip: limit * page - limit,
+      take: limit,
+    });
+  }
+  async getOne(id: string) {
+    return this.prisma.post.findUnique({ where: { id } });
+  }
+  async create(dto: CreatePostDto, userId: string) {
+    const user = await this.userService.findOneById(userId);
+    if (!user) throw new BadRequestException('User doesnot exists');
+    return this.prisma.post.create({
+      data: { title: dto.title, content: dto.content, authorId: user.id },
+    });
+  }
+  async update(dto: UpdatePostDto, postId: string) {
+    return this.prisma.post.update({
+      where: { id: postId },
+      data: { title: dto.title, content: dto.content },
+    });
+  }
+  async delete(id: string) {
+    return this.prisma.post.delete({ where: { id } });
+  }
+}
