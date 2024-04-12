c041af4f7c74c47063e334b0ba50468285dd7619
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-04-12 10:11:37 +0300

    fixed delete post guard

--- lib/guards/post-actions.guard.ts
+++ lib/guards/post-actions.guard.ts
@@ -8,18 +8,21 @@ import { Observable } from 'rxjs';
 import { Request } from 'express';
 import { JwtService } from '@nestjs/jwt';
 import { User } from '@prisma/client';
+import { PostsService } from 'src/posts/posts.service';
 @Injectable()
 export class PostActionsGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(
+    private jwtService: JwtService,
+    private postService: PostsService,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request: Request = context.switchToHttp().getRequest();
       const token = request.headers.authorization.split(' ')[1];
       if (!token) throw new UnauthorizedException();
       const user: User = this.jwtService.verify(token);
-      if (user.id !== request.params.id && !user.roles.includes('ADMIN'))
+      const post = await this.postService.getOne(request.params.id);
+      if (user.id !== post.authorId && !user.roles.includes('ADMIN'))
         throw new UnauthorizedException();
       return true;
     } catch (error) {
